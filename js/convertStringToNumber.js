fa991b5e7001cf9f4ba197e3208e1c7a628e1b06
TatianaYakushonok <yakush2@yandex.ru>
2023-11-07 23:34:29 +0700

    feat: add total amount (Tue, 07 Nov 2023 23:34:29 +0700)

---
+++ js/convertStringToNumber.js
@@ -0,0 +1,10 @@
+export const convertStringToNumber = (str) => {
+  const noSpaceStr = str.replace(/\s+/g, '');
+  const num = parseFloat(noSpaceStr);
+
+  if (!isNaN(num) && isFinite(num)) {
+    return num;
+  } else {
+    return false;
+  }
+};
